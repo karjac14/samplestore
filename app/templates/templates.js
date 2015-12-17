0a869820056beed77607c85273ee5180b0b97102
Karlo Eleuterio <karloe@gmail.com>
2015-12-17 16:26:02 -0500

    angular structure established

---
+++ app/templates/templates.js
@@ -0,0 +1,12 @@
+angular.module('templateStore.templates', ['ngRoute'])
+  .config(['$routeProvider', function ($routeProvider){
+      $routeProvider
+        .when('/templates', {
+          templateUrl: 'templates/templates.html',
+          controller: 'TemplatesCtrl'
+        });
+  }])
+
+  .controller('TemplatesCtrl', [ '$scope', function ($scope) {
+    console.log("test");
+  }])
