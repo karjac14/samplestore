fc54a62e5c2623bb5ae829df0a28163a4b3f4ca3
Karlo Eleuterio <karloe@gmail.com>
2015-12-21 16:32:04 -0500

    main template store working

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -4,9 +4,25 @@ angular.module('templateStore.templates', ['ngRoute'])
         .when('/templates', {
           templateUrl: 'templates/templates.html',
           controller: 'TemplatesCtrl'
+        })
+        .when('/templates/:templateId', {
+          templateUrl: 'templates/templates-details.html',
+          controller: 'TemplatesDtl'
         });
   }])
 
-  .controller('TemplatesCtrl', [ '$scope', function ($scope) {
-    console.log("test");
+  .controller('TemplatesCtrl', [ '$scope', '$http', function ($scope, $http) {
+    $http.get('json/store.json')
+      .then(
+        function(response){
+          $scope.templates = response.data;
+        });
+  }])
+  .controller('TemplatesDtl', [ '$scope', '$http', function ($scope, $http) {
+    $http.get('json/store.json')
+      .then(
+        function(response){
+          $scope.templates = response.data;
+        });
+
   }])
